546eeb446aac0c5828c720704ed5dd327c1c8022
subrato29 <subratosarkar29@gmai.com>
2022-03-13 14:20:43 -0400

    updated code

--- src/main/java/com/playwright/test/PlayWrightBasic.java
+++ src/main/java/com/playwright/test/PlayWrightBasic.java
@@ -1,12 +1,16 @@
 package com.playwright.test;
 
 import com.microsoft.playwright.*;
+import com.microsoft.playwright.BrowserType.LaunchOptions;
 
 public class PlayWrightBasic {
 
 	public static void main(String[] args) {
 		Playwright pw = Playwright.create ();
-		Browser browser = pw.chromium().launch();
+		LaunchOptions lp = new LaunchOptions ();
+		lp.setChannel("chrome");
+		lp.setHeadless(false);
+		Browser browser = pw.chromium().launch(lp);
 		Page page = browser.newPage();
 		page.navigate("https://www.automationexercise.com/");
 		
