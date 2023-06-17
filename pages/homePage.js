aecf69411fcc641307cb4dc5112839dff5ca9309
Subrato Sarkar <ssarkar@SSARKAR2-MBP16.local>
2023-06-17 11:20:45 -0400

    updated code

--- pages/homePage.js
+++ pages/homePage.js
@@ -1,10 +1,22 @@
 export class HomePage {
   constructor(page) {
     this.page = page
-    this.searchTextbox = page.locator("//input[@type = 'search']")
+    this.searchTextbox = page.locator("//input[@id = 'headerSearch']")
+    this.btnSearch = "//button[@id = 'headerSearchButton']"
+    this.searchResultHeader = page.locator(
+      "//div[@class = 'results-header']/div/h1"
+    )
   }
 
   async validatePresenceOfSearchTextbox() {
     return await this.searchTextbox.isVisible()
+  }
+
+  async setSearch(elementToBeSearched) {
+    await this.searchTextbox.type(elementToBeSearched)
+  }
+
+  async validateSearchResultHeader() {
+    return await this.searchResultHeader.textContent()
   }
 }
--- tests/search.spec.js
+++ tests/search.spec.js
@@ -3,7 +3,7 @@ const { HomePage } = require('../pages/homePage')
 
 const BASE_URL = 'https://www.homedepot.com'
 
-test.describe('Validating login page', () => {
+test.describe('Validating search functionality', () => {
   test.beforeEach(async ({ page }) => {
     await page.goto(BASE_URL)
   })
@@ -13,5 +13,15 @@ test.describe('Validating login page', () => {
     const validatePresenceOfSearchTextbox =
       await homePage.validatePresenceOfSearchTextbox()
     expect(validatePresenceOfSearchTextbox).toBe(true)
+  })
+
+  test('Verifying valid search', async ({ page }) => {
+    const homePage = new HomePage(page)
+    const elementToBeSearched = 'Computer table'
+    await homePage.setSearch(elementToBeSearched)
+    await page.click(homePage.btnSearch)
+    homePage.page.waitForTimeout(5000)
+    const actualSearchResult = await homePage.validateSearchResultHeader()
+    expect(actualSearchResult).toBe(elementToBeSearched)
   })
 })
