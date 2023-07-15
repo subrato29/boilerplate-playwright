5f98e4421c41d975afb10af8d5d3a5cc273483dc
Subrato Sarkar <ssarkar@SSARKAR2-MBP16.local>
2023-07-14 21:13:46 -0400

    crud

--- tests/api/crud.spec.js
+++ tests/api/crud.spec.js
@@ -4,14 +4,12 @@ import { TOKEN } from '../../config/token'
 import { urls } from '../../support/readDataFromJson'
 
 test.describe('Verifying champion clothing brand outlet page', () => {
-  const ENDPOINT = `${urls.baseUrl}${urls.users}`
+  let endpoint = `${urls.baseUrl}${urls.users}`
   const random = randomUtils.generateAlphaNumericNoOfCertainLength(6)
   let responseId = ''
 
-  test('Verifying search functionality and add to cart items', async ({
-    request,
-  }) => {
-    const response = await request.fetch(ENDPOINT, {
+  test('POST call', async ({ request }) => {
+    const response = await request.fetch(endpoint, {
       method: 'post',
       headers: {
         'Content-Type': 'application/json',
@@ -27,6 +25,63 @@ test.describe('Verifying champion clothing brand outlet page', () => {
     expect(response.status()).toBe(201)
     const responseBody = await response.json()
     responseId = responseBody.id
-    console.log('Response body: ' + responseId)
+    console.log(JSON.stringify(responseBody))
+  })
+
+  test('GET call', async ({ request }) => {
+    endpoint = `${endpoint}/${responseId}`
+    const response = await request.fetch(endpoint, {
+      method: 'get',
+      headers: {
+        'Content-Type': 'application/json',
+        authorization: `Bearer ${TOKEN}`,
+      },
+    })
+    expect(response.status()).toBe(200)
+    const responseBody = await response.json()
+    responseId = responseBody.id
+  })
+
+  test('PUT call', async ({ request }) => {
+    const response = await request.fetch(endpoint, {
+      method: 'put',
+      headers: {
+        'Content-Type': 'application/json',
+        authorization: `Bearer ${TOKEN}`,
+      },
+      data: {
+        name: `playwright_${random}_Updated`,
+        email: `${random}@email.com`,
+        gender: 'male',
+        status: 'active',
+      },
+    })
+    expect(response.status()).toBe(200)
+    const responseBody = await response.json()
+    console.log(JSON.stringify(responseBody))
+  })
+
+  test('DELETE call', async ({ request }) => {
+    const response = await request.fetch(endpoint, {
+      method: 'delete',
+      headers: {
+        'Content-Type': 'application/json',
+        authorization: `Bearer ${TOKEN}`,
+      },
+    })
+    expect(response.status()).toBe(204)
+  })
+
+  test('GET call to verify successful DELETE', async ({ request }) => {
+    const response = await request.fetch(endpoint, {
+      method: 'get',
+      headers: {
+        'Content-Type': 'application/json',
+        authorization: `Bearer ${TOKEN}`,
+      },
+    })
+    expect(response.status()).toBe(404)
+    const responseBody = await response.json()
+    expect(responseBody.message).toBe('Resource not found')
   })
 })
